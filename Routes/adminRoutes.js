35612fc048436db44f2f960b5a66bbb25dfea84c
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-06-24 20:50:39 +0530

    Tressure Backend Completed

---
+++ Routes/adminRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+// Router Import
+const router = express.Router();
+
+// Utility Import
+// Import Controllers
+const {
+  logAdminIn,
+  createStudentAccounts,
+  createTest,
+  getResults,
+  getAllUsers,
+  resetUserData,
+  resetAllUserData,
+  getTotalQuestionCount,
+  protect,
+} = require("../Controllers/adminControllers");
+
+// Account Login
+router.post("/", logAdminIn);
+// Create Student Account
+router.post("/createStudentAccounts", protect, createStudentAccounts);
+// Get Registered User Data
+router.get("/getAllUsers", protect, getAllUsers);
+// Get Total Question Count
+router.get("/getTotalQuestionCount", protect, getTotalQuestionCount);
+// Reset User Data of a particular User
+router.post("/resetData", protect, resetUserData);
+// Reset All User Data
+router.post("/resetAllUserData", protect, resetAllUserData);
+// Create Test
+router.post("/createTest", protect, createTest);
+// Generate Result
+router.post("/getResults", protect, getResults);
+module.exports = router;
